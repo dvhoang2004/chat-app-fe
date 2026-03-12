49b4af016f737888b27140a572fc3ffcd406821a
dvhoang2004 <dvh122004@gmail.com>
2026-03-12 21:57:04 +0700

    feat: update properties page

--- src/pages/Properties/Properties.jsx
+++ src/pages/Properties/Properties.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
 import './Properties.css'
+import { useUserInfo } from '../../context/userInfoContext'
 
-// This component will display the properties of the offers that the user has made. 
+//user info page, just a simple page to display user info, can be expanded in the future to allow editing user info, changing password, etc.
 const Properties = () => {
+  const { user } = useUserInfo()
+
   return (
-    <div>
-      <p>Properties Page</p>
+    <div className="properties-page">
+      {user.avatar
+        ? <img src={user.avatar} alt="avatar" className="properties-avatar" />
+        : <div className="properties-avatar-placeholder">{user.name.charAt(0)}</div>
+      }
+      <h2>{user.name}</h2>
+      <p>Role: {user.role}</p>
+      <p>Bio: {user.description}</p>
     </div>
   )
 }
